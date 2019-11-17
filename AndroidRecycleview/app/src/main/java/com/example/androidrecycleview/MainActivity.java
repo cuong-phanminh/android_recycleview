575f1ff26dacd5b5daa7d1c83ad93676e2ccd6f5
cuong-phanminh <cuong.phan@student.passerellesnumeriques.org>
2019-11-17 23:06:37 +0700

    commit 2

--- AndroidRecycleview/app/src/main/java/com/example/androidrecycleview/MainActivity.java
+++ AndroidRecycleview/app/src/main/java/com/example/androidrecycleview/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class
+MainActivity extends AppCompatActivity {
 RecyclerView rvNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +21,29 @@ RecyclerView rvNumber;
         rvNumber=findViewById(R.id.rvNumber);
         rvNumber.setLayoutManager(new LinearLayoutManager(this));
 
-        NumberAdapter adapter=new NumberAdapter();
+//        NumberAdapter adapter=new NumberAdapter();
+//        rvNumber.setAdapter(adapter);
+
+        final List<String> data = new ArrayList<>();
+
+        for(int i = 0; i<5 ;i++){
+            data.add("Phong");
+            data.add("Loc");
+            data.add("Toan");
+            data.add("Loan");
+        }
+        final NumberAdapter adapter = new NumberAdapter();
+
+        adapter.data = data;
         rvNumber.setAdapter(adapter);
 
-        final List<String> datal = new ArrayList<>();
-
-        for(int i = 0; i<10 ;i++){
-            datal.add("Phong");
-            datal.add("Loc");
-            datal.add("Toan");
-            datal.add("Loan");
-        }
+        findViewById(R.id.addNew).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data.add("New student");
+                adapter.notifyDataSetChanged();
+            }
+        });
 
     }
 }
